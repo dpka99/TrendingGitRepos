1fe1f7eece344617d3d8863b70facf90d68370e9
dpka@99 <pdpika2012@gmail.com>
2022-04-05 18:50:29 +0530

    Removed workManager initalization code

--- app/src/main/java/com/trendinggitrepos/presentation/App.kt
+++ app/src/main/java/com/trendinggitrepos/presentation/App.kt
@@ -1,13 +1,12 @@
 package com.trendinggitrepos.presentation
 
 import android.app.Application
+import android.util.Log
 import androidx.work.Configuration
-import androidx.work.WorkManager
 import com.trendinggitrepos.workmanager.DaggerWorkerFactory
 import com.trendinggitrepos.presentation.di.AppComponent
 import com.trendinggitrepos.presentation.di.AppModule
 import com.trendinggitrepos.presentation.di.DaggerAppComponent
-
 import javax.inject.Inject
 
 class App : Application(), Configuration.Provider {
@@ -17,22 +16,21 @@ class App : Application(), Configuration.Provider {
     @Inject
     lateinit var workerFactory: DaggerWorkerFactory
 
-
     override fun onCreate() {
         super.onCreate()
         appComponent = DaggerAppComponent.builder()
             .appModule(AppModule(applicationContext)).build()
+
         appComponent.inject(this)
 
-        WorkManager.initialize(this, workManagerConfiguration)
     }
 
     override fun getWorkManagerConfiguration(): Configuration {
-    return Configuration.Builder()
-        .setWorkerFactory(workerFactory)
-    .setMinimumLoggingLevel(android.util.Log.DEBUG)
-    .build()
-}
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .setMinimumLoggingLevel(android.util.Log.DEBUG)
+            .build()
+    }
 
 
 }
